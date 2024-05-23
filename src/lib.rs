a169dfb9ac911dcb24288d102916a8c44a4034fd
damccull <401104+damccull@users.noreply.github.com>
2024-05-23 09:30:07 -1000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+trait TreeNode {}
+
+pub struct DocumentTree {
+    leaves: Vec<Box<dyn TreeNode>>,
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+}
+
+fn main() {}
